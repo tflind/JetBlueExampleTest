18b17eb232373eebb02a3d54847330571a29de31
tmflinders <tmflinders@yahoo.com>
2017-12-22 11:02:18 -0700

    created a the spec.js file and added a sample test to make sure it's working.

---
+++ JetBlueExampleTest/spec.js
@@ -0,0 +1,6 @@
+﻿describe('jetblue homepage', function () {
+    it('should have a title', function () {
+        browser.get('http://www.jetblue.com/#/');
+        expect(browser.getTitle()).toEqual('JetBlue | Airline Tickets, Flights, and Airfare');
+    });
+});
