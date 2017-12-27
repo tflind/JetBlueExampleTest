03f88e03e187fc238d5e81a643b17ea6bcd0b294
tmflinders <tmflinders@yahoo.com>
2017-12-27 10:53:12 -0700

    updated the test to include email sign in

--- JetBlueExampleTest/spec.js
+++ JetBlueExampleTest/spec.js
@@ -1,6 +1,20 @@
 ﻿describe('jetblue homepage', function () {
-    it('should have a title', function () {
+    var email = 
+    beforeEach(function () {
         browser.get('http://www.jetblue.com/#/');
-        expect(browser.getTitle()).toEqual('JetBlue | Airline Tickets, Flights, and Airfare');
     });
+        it('should have a title', function () {
+           
+            expect(browser.getTitle()).toEqual('JetBlue | Airline Tickets, Flights, and Airfare');
+        });
+        it('should allow user to sign in', function () {
+            element(by.id('email_field')).sendKeys('tfxact@gmail.com');
+            element(by.id('password_field')).sendKeys(8019328746);
+
+            element(by.id('signin_btn')).click();
+
+        });
+
+       
+
 });
